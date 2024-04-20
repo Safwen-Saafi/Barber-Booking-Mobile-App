8fff841908ff4a7ef5b0a9ef2b50fbf9b92370b7
Safwen-Saafi <saafi.saf0@gmail.com>
2024-04-20 14:54:57 +0100

    Added custom splash screen

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -12,23 +12,6 @@ class Onboarding extends StatelessWidget {
           child: Column(children: [
             Image.asset("assets/images/single_dark.jpg"),
             const SizedBox(height: 50.0),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: <Color>[
-                    Color(0xFFbc4839),
-                    Color.fromARGB(255, 179, 0, 0),
-                    Color.fromARGB(255, 190, 69, 13)
-                  ]),
-                  //color: Color(0xFFbc4839),
-                  borderRadius: BorderRadius.circular(30)),
-              child: Text("Get your stylish haircut",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Poppins',
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.bold)),
-            ),
           ]),
         ));
   }
