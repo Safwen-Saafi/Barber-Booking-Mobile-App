1aaf4ab31a39b67c281ad67e7896156d7899ca1c
Safwen-Saafi <saafi.saf0@gmail.com>
2024-04-25 20:39:52 +0100

    Final version of splash screen

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\hp\Documents\MY REPOS\barber_app"
+export "FLUTTER_APPLICATION_PATH=C:\Users\hp\Documents\MY REPOS\Barber-Booking-Mobile-App"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
---
+++ lib/onboarding_pages/onboarding_page_1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:barber_app/utils/constants/colors.dart';
+
+class IntroPage1 extends StatelessWidget {
+  const IntroPage1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: 
+        BoxDecoration(
+          gradient: BColors.tupac
+          ),
+        );
+  }
+}
